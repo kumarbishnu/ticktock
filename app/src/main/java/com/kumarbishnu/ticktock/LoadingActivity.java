95c252a51def94b6673f8b0e128b4de969e7ad63
kumar <kumarbishnu202@gmail.com>
2020-08-27 11:23:11 +0545

    init app

---
+++ app/src/main/java/com/kumarbishnu/ticktock/LoadingActivity.java
@@ -0,0 +1,17 @@
+package com.kumarbishnu.ticktock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+}
